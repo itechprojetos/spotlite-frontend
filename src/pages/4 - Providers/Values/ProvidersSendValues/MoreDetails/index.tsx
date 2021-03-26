c23805a25091820df22ebd4bc135b6fedee607d3
Clayderson Ferreira <clayderson2010@gmail.com>
2021-03-26 06:33:44 -0300

    Finaliazndo layout do modal de informacoes; aguardando endpoint para desenvolver parte funcional

--- src/pages/4 - Providers/Values/ProvidersSendValues/MoreDetails/index.tsx
+++ src/pages/4 - Providers/Values/ProvidersSendValues/MoreDetails/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Grid, Box, Typography } from '@material-ui/core';
 import {
-  Container, CloseModalButton, Tabs, Tab,
+  Container, CloseModalButton, Tabs, Tab, InputTextField,
 } from './style';
 
 interface TabPanelProps {
@@ -58,31 +58,427 @@ const MoreDetails: React.FC<MoreDetails> = ({ toggleHandler = false }) => {
             onChange={(e, newValue): void => setCurrentTab(newValue)}
           >
             <Tab label="Visão geral" {...a11yProps(0)} />
-            <Tab label="Peso e quantidade" {...a11yProps(1)} />
-            <Tab label="Dimensões" {...a11yProps(2)} />
-            <Tab label="DGR" {...a11yProps(3)} />
-            <Tab label="Mercadoria" {...a11yProps(4)} />
-            <Tab label="Outras informações" {...a11yProps(5)} />
+            <Tab label="Coleta e entrega" {...a11yProps(1)} />
+            <Tab label="Peso e quantidade" {...a11yProps(2)} />
+            <Tab label="Dimensões" {...a11yProps(3)} />
+            <Tab label="DGR" {...a11yProps(4)} />
+            <Tab label="Mercadoria" {...a11yProps(5)} />
+            <Tab label="Outras informações" {...a11yProps(6)} />
           </Tabs>
         </Grid>
-        <Grid item xs={9}>
+        <Grid item xs={9} style={{ borderLeft: '1px solid #e3e3e3', minHeight: '500px' }}>
           <TabPanel value={currentTab} index={0}>
-            Visão geral
+            <Box component="p" mb={4}>
+              Visão geral
+            </Box>
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs={12}>
+                <InputTextField
+                  label="Título"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="Cotação de Israel"
+                />
+              </Grid>
+            </Grid>
+            <Box mb={2} />
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs={6}>
+                <InputTextField
+                  label="Tipo"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="Nacional"
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <InputTextField
+                  label="Modal"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="Aéreo"
+                />
+              </Grid>
+            </Grid>
+            <Box mb={2} />
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs={6}>
+                <InputTextField
+                  label="Nível de serviço"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="Consol"
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <InputTextField
+                  label="Seguro"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="Nenhum"
+                />
+              </Grid>
+            </Grid>
           </TabPanel>
           <TabPanel value={currentTab} index={1}>
-            Peso e quantidade
+            <Box component="p" mb={4}>
+              Coleta e entrega
+            </Box>
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs={6}>
+                <InputTextField
+                  label="Aeroporto de origem"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="GRU Guarulhos"
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <InputTextField
+                  label="Aeroporto de destino"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="REC Recife"
+                />
+              </Grid>
+            </Grid>
+            <Box component="p" mt={3} mb={1}>
+              Local de coleta
+            </Box>
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs>
+                <InputTextField
+                  label="Local"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+              <Grid item xs>
+                <InputTextField
+                  label="Endereço"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+            </Grid>
+            <Box mb={2} />
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs>
+                <InputTextField
+                  label="CEP"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+              <Grid item xs>
+                <InputTextField
+                  label="Cidade"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+              <Grid item xs>
+                <InputTextField
+                  label="Estado"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+            </Grid>
+            <Box component="p" mt={3} mb={1}>
+              Local de entrega
+            </Box>
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs>
+                <InputTextField
+                  label="Local"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+              <Grid item xs>
+                <InputTextField
+                  label="Endereço"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+            </Grid>
+            <Box mb={2} />
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs>
+                <InputTextField
+                  label="CEP"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+              <Grid item xs>
+                <InputTextField
+                  label="Cidade"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+              <Grid item xs>
+                <InputTextField
+                  label="Estado"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+            </Grid>
           </TabPanel>
           <TabPanel value={currentTab} index={2}>
-            Dimensões
+            <Box component="p" mb={4}>
+              Peso e quantidade
+            </Box>
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs>
+                <InputTextField
+                  label="Peso bruto total"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="20 KG"
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <InputTextField
+                  label="Quantidade"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="5"
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <InputTextField
+                  label="Empilhável"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="Sim"
+                />
+              </Grid>
+            </Grid>
+            <Box mb={2} />
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs>
+                <InputTextField
+                  label="Peso bruto total"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="55 KG"
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <InputTextField
+                  label="Quantidade"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="3"
+                />
+              </Grid>
+              <Grid item xs={3}>
+                <InputTextField
+                  label="Empilhável"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="Não"
+                />
+              </Grid>
+            </Grid>
           </TabPanel>
           <TabPanel value={currentTab} index={3}>
-            DGR
+            <Box component="p" mb={4}>
+              Dimensões
+            </Box>
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs>
+                <InputTextField
+                  label="Comprimento"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="50 CM"
+                />
+              </Grid>
+              <Grid item xs>
+                <InputTextField
+                  label="Largura"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="39 CM"
+                />
+              </Grid>
+              <Grid item xs>
+                <InputTextField
+                  label="Altura"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="11 CM"
+                />
+              </Grid>
+            </Grid>
+            <Box mb={2} />
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs>
+                <InputTextField
+                  label="Comprimento"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="23 CM"
+                />
+              </Grid>
+              <Grid item xs>
+                <InputTextField
+                  label="Largura"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="45 CM"
+                />
+              </Grid>
+              <Grid item xs>
+                <InputTextField
+                  label="Altura"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="20 CM"
+                />
+              </Grid>
+            </Grid>
           </TabPanel>
           <TabPanel value={currentTab} index={4}>
-            Mercadoria
+            <Box component="p" mb={4}>
+              DGR
+            </Box>
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs={6}>
+                <InputTextField
+                  label="Classes"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <InputTextField
+                  label="UN"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+            </Grid>
+            <Box mb={2} />
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs={6}>
+                <InputTextField
+                  label="Temperatura mínima (°C)"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <InputTextField
+                  label="Temperatura máxima (°C)"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                />
+              </Grid>
+            </Grid>
           </TabPanel>
           <TabPanel value={currentTab} index={5}>
-            Outras informações
+            <Box component="p" mb={4}>
+              Mercadoria
+            </Box>
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs={8}>
+                <InputTextField
+                  label="Descrição"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="Notebook HP i7 16GB 120GB SSD"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <InputTextField
+                  label="Valor"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="R$ 5.490,55"
+                />
+              </Grid>
+            </Grid>
+            <Box mb={2} />
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs={8}>
+                <InputTextField
+                  label="Descrição"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="Monitor LG Ultrawide 29'"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <InputTextField
+                  label="Valor"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  defaultValue="R$ 1.155,00"
+                />
+              </Grid>
+            </Grid>
+          </TabPanel>
+          <TabPanel value={currentTab} index={6}>
+            <Box component="p" mb={4}>
+              Outras informações
+            </Box>
+            <Grid container xs={12} spacing={2}>
+              <Grid item xs={12}>
+                <InputTextField
+                  label="Observações"
+                  variant="outlined"
+                  size="small"
+                  inputProps={{ readOnly: true }}
+                  rows={7}
+                  multiline
+                  defaultValue="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                />
+              </Grid>
+            </Grid>
           </TabPanel>
         </Grid>
       </Grid>
--- src/pages/4 - Providers/Values/ProvidersSendValues/MoreDetails/style.ts
+++ src/pages/4 - Providers/Values/ProvidersSendValues/MoreDetails/style.ts
@@ -2,6 +2,13 @@ import styled from 'styled-components';
 import * as material from '@material-ui/core';
 import { FaTimes } from 'react-icons/fa';
 
+export const CloseModalButton = styled(FaTimes)`
+  position: absolute;
+  top: 25px;
+  right: 25px;
+  cursor: pointer;
+`;
+
 export const Container = styled.div`
   position: absolute;
   padding: 2rem;
@@ -16,15 +23,7 @@ export const Container = styled.div`
   box-shadow: 0px 0px 81px 3px rgba(0,0,0,0.15);
 `;
 
-export const CloseModalButton = styled(FaTimes)`
-  position: absolute;
-  top: 25px;
-  right: 25px;
-  cursor: pointer;
-`;
-
 export const Tabs = styled(material.Tabs)`
-  border-right: 1px solid #e3e3e3;
 `;
 
 export const Tab = styled(material.Tab)`
@@ -32,5 +31,18 @@ export const Tab = styled(material.Tab)`
     align-items: left;
     text-align: left;
     display: initial;
+  }
+`;
+
+export const InputTextField = styled(material.TextField)`
+  width: 100%;
+  border-radius: 4px;
+
+  .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline {
+    border-color: #767676;
+  }
+
+  .MuiInputLabel-outlined.Mui-focused {
+    color: #767676;
   }
 `;
